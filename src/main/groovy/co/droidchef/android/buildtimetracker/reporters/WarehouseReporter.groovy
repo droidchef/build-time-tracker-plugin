ec633b8ba4c166a452ddecdc5adf5418f5edf08b
Ishan Khanna <ishan1604@gmail.com>
2019-05-16 15:18:09 +0200

    Improves data being passed by the Warehouse Reporter by clubbing meta data from measurements into one single object and releases version 1.0.3

--- src/main/groovy/co/droidchef/android/buildtimetracker/reporters/WarehouseReporter.groovy
+++ src/main/groovy/co/droidchef/android/buildtimetracker/reporters/WarehouseReporter.groovy
@@ -1,66 +1,54 @@
 package co.droidchef.android.buildtimetracker.reporters
 
 import co.droidchef.android.buildtimetracker.Timing
+import groovyx.net.http.ContentType
 import groovyx.net.http.HTTPBuilder
-import net.sf.json.JSON
+import groovyx.net.http.Method
+import org.gradle.api.logging.Logger
 
 import java.text.DateFormat
 import java.text.SimpleDateFormat
-import org.gradle.api.logging.Logger
 
 class WarehouseReporter extends AbstractBuildTimeTrackerReporter {
     WarehouseReporter(HashMap<String, String> options, Logger logger) {
         super(options, logger)
     }
 
-    List measurements(List<Timing> timings) {
-        long timestamp = new TrueTimeProvider().getCurrentTime()
+    @Override
+    def run(List<Timing> timings) {
 
+        long timestamp = new TrueTimeProvider().getCurrentTime()
         TimeZone tz = TimeZone.getTimeZone("UTC")
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss,SSS'Z'")
         df.setTimeZone(tz)
-
-        def hostname = "unknown"
-        try {
-            InetAddress localhost = InetAddress.getLocalHost()
-            hostname = localhost.getHostName()
-        } catch (UnknownHostException exception) {
-            // do nothing
-        }
 
         def info = new SysInfo()
         def osId = info.getOSIdentifier()
         def cpuId = info.getCPUIdentifier()
         def maxMem = info.getMaxMemory()
         def measurements = []
-        def ultimateSuccess = timings.every { it.success }
-        def userName = System.getProperty("user.name");
+
         timings.eachWithIndex { it, index ->
             measurements << [
-                    timestamp       : timestamp,
-                    order           : index,
-                    task            : it.path,
-                    success         : it.success,
-                    did_work        : it.didWork,
-                    skipped         : it.skipped,
-                    ms              : it.ms,
-                    cpu             : cpuId,
-                    memory          : maxMem,
-                    os              : osId,
-                    hostname        : hostname,
-                    git_sha         : getOption('git', ''),
-                    ultimate_success: ultimateSuccess,
-                    user_name       : userName
+                    timestamp: timestamp,
+                    order: index,
+                    task: it.path,
+                    success: it.success,
+                    did_work: it.didWork,
+                    skipped: it.skipped,
+                    ms: it.ms,
+                    date: df.format(new Date(timestamp)),
+                    cpu: cpuId,
+                    memory: maxMem,
+                    os: osId,
             ]
         }
 
-        return measurements
-    }
-
-    @Override
-    def run(List<Timing> timings) {
-        def measurements = measurements(timings)
-
+        def data = [
+                success: timings.every { it.success },
+                count: timings.size(),
+                measurements: measurements,
+        ]
         // write to server
         def url = getOption("url", null)
         def http = new HTTPBuilder(url)
@@ -68,8 +56,8 @@ class WarehouseReporter extends AbstractBuildTimeTrackerReporter {
         http.getClient().getParams().setParameter("http.socket.timeout", new Integer(5000))
 
         try {
-            http.request(POST, JSON) { req ->
-                body = measurements
+            http.request(Method.POST, ContentType.JSON) { req ->
+                body = data
 
                 //headers.put(getOption('headerParamKey1', null), getOption('headerParamVal1', null))
                 headers.Accept = 'application/json'
