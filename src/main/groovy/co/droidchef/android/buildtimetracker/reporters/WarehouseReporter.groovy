e1bd389c3865b073afedbaa1d21e8c762ee6d4f4
Ishan Khanna <ishan1604@gmail.com>
2019-07-31 15:41:45 +0200

    Released v1.0.13, with firstTaskNames being pushed to the endpoint

--- src/main/groovy/co/droidchef/android/buildtimetracker/reporters/WarehouseReporter.groovy
+++ src/main/groovy/co/droidchef/android/buildtimetracker/reporters/WarehouseReporter.groovy
@@ -6,6 +6,7 @@ import groovyx.net.http.HTTPBuilder
 import groovyx.net.http.Method
 import org.apache.http.conn.scheme.Scheme
 import org.apache.http.conn.ssl.SSLSocketFactory
+import org.gradle.BuildResult
 import org.gradle.api.logging.Logger
 
 import javax.net.ssl.SSLContext
@@ -17,13 +18,18 @@ import java.text.DateFormat
 import java.text.SimpleDateFormat
 
 class WarehouseReporter extends AbstractBuildTimeTrackerReporter {
+
+    def data = []
+    def metaData = []
+    def debugHttp = false
+
     WarehouseReporter(HashMap<String, String> options, Logger logger) {
         super(options, logger)
     }
 
     @Override
     def run(List<Timing> timings) {
-
+        debugHttp = getOption("debugHttp", "false").toBoolean()
         long timestamp = new TrueTimeProvider().getCurrentTime()
         TimeZone tz = TimeZone.getTimeZone("UTC")
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss,SSS'Z'")
@@ -38,6 +44,7 @@ class WarehouseReporter extends AbstractBuildTimeTrackerReporter {
         def measurements = []
 
         timings.eachWithIndex { it, index ->
+            logger.lifecycle(it.path)
             measurements << [
                     timestamp: timestamp,
                     order: index,
@@ -50,32 +57,39 @@ class WarehouseReporter extends AbstractBuildTimeTrackerReporter {
             ]
         }
 
-        def data = [
+        metaData = [
+                cpu: cpuId,
+                memory: maxMem,
+                os: osId,
+                username: userName
+        ]
+
+        data = [
                 success: timings.every { it.success },
                 count: timings.size(),
-                meta: [
-                        cpu: cpuId,
-                        memory: maxMem,
-                        os: osId,
-                        username: userName
-                ],
+                meta: metaData,
                 measurements: measurements,
         ]
-        // write to server
+    }
+
+    private void postDataToWarehouse(data, boolean debugHttp) {
         def url = getOption("url", null)
         def http = new HTTPBuilder(url)
         http.getClient().getParams().setParameter("http.connection.timeout", new Integer(5000))
         http.getClient().getParams().setParameter("http.socket.timeout", new Integer(5000))
         try {
             SSLContext sc = SSLContext.getInstance("SSL")
-            sc.init(null, [ new X509TrustManager() {
-                public X509Certificate[] getAcceptedIssuers() {null }
-                public void checkClientTrusted(X509Certificate[] certs, String authType) { }
-                public void checkServerTrusted(X509Certificate[] certs, String authType) { }
-            } ] as TrustManager[], new SecureRandom())
+            sc.init(null, [new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() { null }
+
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+            }] as TrustManager[], new SecureRandom())
             def sf = new SSLSocketFactory(sc, SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER)
-            def httpsScheme = new Scheme("https", sf, 80)
+            def httpsScheme = new Scheme("https", sf, 443)
             http.client.connectionManager.schemeRegistry.register(httpsScheme)
+
 
             http.request(Method.POST, ContentType.JSON) { req ->
                 body = data
@@ -84,15 +98,42 @@ class WarehouseReporter extends AbstractBuildTimeTrackerReporter {
                 headers.Accept = 'application/json'
 
                 response.success = { resp, json ->
+                    if (debugHttp) {
+                        logger.lifecycle(json)
+                    }
                     logger.quiet 'Build stats reported'
                 }
                 response.failure = { resp ->
+                    if (debugHttp) {
+                        logger.lifecycle resp.toString()
+                    }
                     logger.quiet 'Failed to report build stats!'
                 }
             }
         } catch (Exception exception) {
             logger.quiet sprintf('Failed to report build stats! %1$s', exception.toString())
+            logger.lifecycle(exception.stackTrace.toString())
             //logger.quiet sprintf(data.toMapString())
         }
+    }
+
+    @Override
+    void onBuildResult(BuildResult result) {
+
+        def taskList = result.gradle.startParameter.taskRequests
+
+        def firstTaskName = "undefined"
+
+        if (taskList != null && !taskList.isEmpty()) {
+            def arguments = taskList.get(0).args
+            if (arguments.size() > 0) {
+                firstTaskName = arguments.get(0)
+            }
+        }
+
+        metaData.put("firstTaskName", firstTaskName)
+
+        // write to server
+        postDataToWarehouse(data, debugHttp)
     }
 }
